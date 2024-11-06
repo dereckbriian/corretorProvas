d732c0640fba5e2db0e68909cabe5ccc017439dd
dereck <dereckbriian@gmail.com>
2024-11-06 10:34:33 -0300

    adicionando projeto

---
+++ src/main/java/br/com/corretorProvas/CorretorProvasApplication.java
@@ -0,0 +1,13 @@
+package br.com.corretorProvas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorretorProvasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorretorProvasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/corretorProvas/CorretorProvasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.corretorProvas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorretorProvasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
