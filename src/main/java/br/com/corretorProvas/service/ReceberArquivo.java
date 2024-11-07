2d71c5809d3ffc6fb5c81403267e6324043e96c0
dereck <dereckbriian@gmail.com>
2024-11-07 14:36:03 -0300

    Front

---
+++ src/main/java/br/com/corretorProvas/service/ReceberArquivo.java
@@ -0,0 +1,13 @@
+package br.com.corretorProvas.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReceberArquivo {
+
+
+
+    public String redirecionar() {
+        return "https://www.google.com";
+    }
+}
